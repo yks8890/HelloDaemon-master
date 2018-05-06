25b66fadd3c43392daff47dd370d93169e852c1c
yks8890 <yks8890@hanmail.net>
2018-05-06 19:38:05 +0900

    번역

--- sample/src/main/java/com/xdandroid/sample/App.java
+++ sample/src/main/java/com/xdandroid/sample/App.java
@@ -9,7 +9,7 @@ public class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        //需要在 Application 的 onCreate() 中调用一次 DaemonEnv.initialize()
+        //   응용 프로그램의 onCreate ()에서 DaemonEnv.initialize() 한 번 호출해야합니다.
         DaemonEnv.initialize(this, TraceServiceImpl.class, DaemonEnv.DEFAULT_WAKE_UP_INTERVAL);
         TraceServiceImpl.sShouldStopService = false;
         DaemonEnv.startServiceMayBind(TraceServiceImpl.class);
